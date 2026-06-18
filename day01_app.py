aa8262e514c4a8a2a1734c2bf88687693cf7a835
Vidhya S <vidhyared652007@gmail.com>
2026-06-18 21:31:38 +0530

    updated day02_

---
+++ day01_app.py
@@ -0,0 +1,54 @@
+
+"""
+#hello world
+print("Hello World")
+"""
+
+"""
+a=int(input())
+b=(int(input()))
+#add two numbers
+print(a+b)
+#sub two numbers
+print(a-b)
+#multiply two numbers
+print(a*b)
+#divide two numbers
+print(a/b)
+"""
+
+
+"""
+#square of a number
+a=int(input())
+print(a*a)
+#cube of a number
+print(a*a*a)
+"""
+"""
+#check even or odd
+a=int(input())
+if(a%2==0):
+    print("The given number is Even")
+else:
+    print("The given number is Odd")
+    """
+"""
+#swap two numbers
+a=int(input())
+b=int(input())
+temp=a
+a=b
+b=temp
+print(a,b)
+"""
+"""
+#largest of two numbers
+a=int(input())
+b=int(input())
+if(a>b):
+    print(a)
+else:
+    print(b)
+    """
+    
---
+++ day02_app.py
@@ -0,0 +1,78 @@
+"""
+#largest of two numbers
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+if a>b:
+  print("largest value is:",a)
+else:
+  print("largest value is:",b)
+"""
+"""
+#even or odd numbers
+num= int(input("enter the value:"))
+if (num %2==0):
+    print("the number is even")
+else:
+    print("the number is odd")
+"""  
+"""
+#prime numbers
+num=(int(input()))
+if num>1:
+    for i in range(2, num):
+        if num % i==0:
+            print("Not Prime")
+            break
+    else:
+        print("prime")
+else:
+    print("not prime")
+    """
+"""
+# or 
+num=int(input("enter the value:"))
+count=0
+for i in range(2,num+1):
+    if num % i==0:
+        count+=1
+if count==2:
+    print("prime")
+else:
+    print("Not prime")
+    """
+"""
+# palindrome
+num=(input("enter the value:"))
+if num==num[ : : -1]:
+    print("palindrome ")
+else:
+    print("not palindrome")  
+    """
+"""
+ #multiplication table
+n=int(input("enter a number:"))
+for i in range(1,11):
+    print(i,"*",n,"=",n*i) 
+"""  
+"""
+num=int(input("enter the value:"))
+if num<0:
+    print("Negative")
+elif(num>0):
+    print("Positive")
+else:
+    print("Zero")
+    """
+"""
+#square of the number
+num=int(input("enter a number:"))
+print(num*num)
+"""
+"""
+#count characters in string
+text=input("enter the word:")
+print("length=",len(text))
+"""
+
+
+    
