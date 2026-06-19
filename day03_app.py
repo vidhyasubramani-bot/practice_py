474e93b1d278bea7043b8222290d84b0eab7f2f6
Vidhya S <vidhyared652007@gmail.com>
2026-06-19 20:57:38 +0530

    added day 3 python practice program

--- day03_app.py
+++ day03_app.py
@@ -32,3 +32,39 @@ n=int(input("enter the value:"))
 for i in range(1,n+1):
  print("*"  * i)
  """
+"""
+ #largest of three numbers
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+c=int(input("enter the value:"))
+if(a>b and a>c):
+    print("a is largest number")
+elif(b>a and b>c):
+    print(" b is largest number")
+else:
+    print("c is largest number")
+"""
+"""
+#leap year
+year=int(input("enter the year:"))
+if(year%4==0):
+    print("leap year")
+else:
+    print("not a leap year")
+    """
+"""
+# sum of first n numbers
+num=(int(input())) 
+sum=0
+for i in range(1,num+1):
+    sum +=i
+print("Sum:",sum)
+"""
+"""
+#reversed a string
+text=input()
+print("reversed:",text[::-1])
+"""
+ 
+
+
