9f563001f687f25be286fa4c55d351d81c471125
Vidhya S <vidhyared652007@gmail.com>
2026-06-19 07:44:11 +0530

    update day03_app.py

---
+++ day03_app.py
@@ -0,0 +1,34 @@
+"""
+#check even or odd
+num=int(input("enter the value:"))
+if(num/2==0):
+  print("Even")
+else:
+    print("odd")
+    """
+"""
+#print n numbers
+n=int(input())
+for i in range(1,n+1):
+    print(i)
+    """
+"""
+#sum of first n numbers
+num=int(input("enter the value:"))
+n=0
+for i in range(1,num+1):
+    n+=i
+print("sum=",n)
+"""
+"""
+#multiplication table
+num=int(input())
+for i in range(1,11):
+    print(i,"*",num,"=",i*num)
+    """
+"""
+#simple pattern
+n=int(input("enter the value:"))
+for i in range(1,n+1):
+ print("*"  * i)
+ """
