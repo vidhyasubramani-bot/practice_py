e5037b8cacf1beedcc0ad38272e12664a85c01ae
Vidhya S <vidhyared652007@gmail.com>
2026-06-21 11:41:50 +0530

    Added Day 4 Python practice programs

---
+++ day05_app.py
@@ -0,0 +1,63 @@
+"""
+#name and age
+name="Vidhya"
+age=19
+print(f" my name is {name}, i am {age} years old")
+"""
+
+"""
+#square of the number
+num=int(input("enter the number:"))
+print(num*num)
+"""
+"""
+# area of triangle
+length=int(input("enter the length:"))
+breadth=int(input("enter the breadth:"))
+print(length*breadth)
+"""
+"""
+#celsius to fahrenheit
+celsius=int(input("enter the celsius:"))
+fahrenheit=((celsius*9/5)+32)
+print(fahrenheit)
+"""
+"""
+#swap two variables without using third variable
+a=int(input("enter the number:"))
+b=int(input("enter the value:"))
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+"""
+"""
+p=int(input("enter the principle:"))
+r=int(input("enter the rate:"))
+t=int(input("enter the time"))
+SI=(p*r*t/100)
+print(SI)
+"""
+"""
+# total marks and percentege
+t=int(input("enter the tamil mark:"))
+e=int(input("enter the english mark:"))
+m=int(input("enter the maths mark:"))
+s=int(input("enter the maths science:"))
+ss=int(input("enter the maths social:"))
+total=t+e+m+s+ss
+percentage=total/5
+print(percentage)
+"""
+"""
+#average of three numbers
+m1=int(input())
+m2=int(input())
+m3=int(input())
+print("average:",m1+m2+m3/3)
+"""
+minutes=int(input("enter the minutes:"))
+hours=minutes//60
+remaining_minutes=minutes%60
+print(hours,"hours",remaining_minutes,"minutes")
