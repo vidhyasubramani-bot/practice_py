b9affe8210699f1b4857b14b20c058a3c898f236
Vidhya S <vidhyared652007@gmail.com>
2026-06-23 06:30:25 +0530

    updated day6

--- day06_app.py
+++ day06_app.py
@@ -1,12 +1,57 @@
-n = 5
+"""
+#assign multiple values
+a,b,c=10,20,30
+print(a,b,c)
+"""
+"""
+# assign same values
+x=y=z=100
+print(x,y,z)
+"""
+"""
+#variables together
+name="python"
+version=3.12
+print("language:",name,"version:",version)
+"""
+"""
+#global variable
+x=20
+def my_function():
+    print("inside function:",x)
+my_function()
+print("outside function:",x)
 
-for i in range(2*n-1):
-    for j in range(2*n-1):
-        if ((i==0 and j not in [0,n-1,2*n-2]) or
-            (i==1 and j in [0,n-2,n,2*n-2]) or
-            (i-j==2) or
-            (i+j==2*n+2)):
-            print("*", end=" ")
-        else:
-            print(" ", end=" ")
-    print()
+"""
+"""
+#changing global variable
+x=10
+def change_value():
+    global x
+    x=99
+change_value()
+print(x)
+"""
+"""
+# sum of digits
+num=int(input("enter the value:"))
+count=0
+while num>0:
+    digit=num%10
+    count+=digit
+    num=num//10
+print(count)
+
+"""
+"""
+# reverse the number
+n=int(input("enter the value:"))
+reverse=0
+while n>0:
+    digit=n%10
+    reverse=reverse*10+digit
+    n=n//10
+print(reverse)
+"""
+    
+
