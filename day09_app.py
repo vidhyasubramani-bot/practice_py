eb27803b5c7f11b89d81855e777ef68c449a40cb
Vidhya S <vidhyared652007@gmail.com>
2026-06-25 21:27:18 +0530

    day9 python

---
+++ day09_app.py
@@ -0,0 +1,57 @@
+"""
+#membership operator
+fruits=["apple","orange","banana"]
+print("apple" in  fruits)
+print("orange" in fruits)
+print("apple" not in fruits)
+"""
+"""
+#bitwise operators
+a=56
+b=98
+print(a&b)
+print(a|b)
+print(a^b)
+print(a<<b)
+print(a>>b)
+"""
+"""
+#operator precendence
+result=2+6*(3+8)
+print(result)
+"""
+"""
+num=int(input("enter the value:"))
+number_list=[2,5,4,6,7]
+if num in number_list:
+    print("nmu present in number_list")
+else:
+    print("number is not present in number_list")
+    
+result=num & 7
+print(result)
+n=10+5*2-3
+print(n)
+"""
+"""
+#membership operator
+students=["vidhya","pooja","savi","vaishu"]
+name=input("enter the name:")
+if name in students:
+    print("students found")
+else:
+    print("students not found")
+"""
+"""
+#bitwise operator
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+print(a&b)
+"""
+"""
+#left shift and right shift
+a=int(input("enter the value:"))
+print(a>>2)
+print(a<<2)
+"""
+
