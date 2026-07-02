11e71137d9dcefd5db61160ed8b399c45c1bce41
Vidhya S <vidhyared652007@gmail.com>
2026-07-02 07:29:24 +0530

    day15 python

---
+++ day15_app.py
@@ -0,0 +1,25 @@
+"""
+# function 
+def message():
+    print("i am vidhya")
+message()
+
+    """
+"""
+#find square  using function 
+def square(num):
+    print(num*num)
+square(19)
+"""
+"""
+#list
+colors=["red","green","blue"]
+print(colors[0])
+colors[1]="Yellow"
+print(colors)
+colors.extend(["purple"])
+print(colors)
+colors.remove("red")
+print(colors)
+
+    """
